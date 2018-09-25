f83c3c4069f1f32ec22e987e20951eb43fc68aea
15280970040 <15280970040@163.com>
2018-09-25 23:01:12 +0800

    '删除信息'

--- Config/WeChat.config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    AppId:"wxf936bf9c99101a08",
-    AppSecret:"4aee036edc95d7a1a61c053259f215d4",
-    Token: 'mytoken',
-    URL: ' http://8vmyk8.natappfree.cc'
-}
-
-module.exports = config
